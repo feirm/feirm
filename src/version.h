3bec8a0b2e1f78ef200ada56c8266488416013bf
konez2k <konez2k@gmail.com>
2018-03-02 12:29:18 +0100

    updated client version and spork

--- src/version.h
+++ src/version.h
@@ -11,7 +11,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 70912;
+static const int PROTOCOL_VERSION = 70913;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
@@ -22,6 +22,7 @@ static const int GETHEADERS_VERSION = 70077;
 //! disconnect from peers older than this proto version
 static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70911;
 static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70912;
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT3 = 70913;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
@@ -35,6 +36,5 @@ static const int MEMPOOL_GD_VERSION = 60002;
 
 //! "filter*" commands are disabled without NODE_BLOOM after and including this version
 static const int NO_BLOOM_VERSION = 70005;
-
 
 #endif // BITCOIN_VERSION_H
