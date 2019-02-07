14ae25931972347d79be9c5a88cbf9d8a32dda4a
Jack <jackkdev@protonmail.com>
2019-02-07 19:54:28 +0000

    Fix enforcement

--- src/main.cpp
+++ src/main.cpp
@@ -5474,15 +5474,15 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
 int ActiveProtocol()
 {
     if (IsSporkActive(SPORK_20_NEW_PROTOCOL_ENFORCEMENT_6)) {
-        return MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT_4;
-    }
-
-    if (IsSporkActive(SPORK_19_NEW_PROTOCOL_ENFORCEMENT_5)) {
         return MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT_3;
     }
 
-    if (IsSporkActive(SPORK_18_NEW_PROTOCOL_ENFORCEMENT_4)) {
+    if (IsSporkActive(SPORK_19_NEW_PROTOCOL_ENFORCEMENT_5)) {
         return MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT_2;
+    }
+
+    if (IsSporkActive(SPORK_18_NEW_PROTOCOL_ENFORCEMENT_4)) {
+        return MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT;
     }
 
     return MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT;
--- src/version.h
+++ src/version.h
@@ -23,7 +23,8 @@ static const int GETHEADERS_VERSION = 70077;
 static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70917;
 static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT_2 = 70918;
 static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT_3 = 70919;
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT_4 = 70920;
+
+static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70916;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
