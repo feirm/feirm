87522a2c350a219add7d62872fcb97fbc3407754
Jack Coble <jackcoble@tutanota.com>
2019-11-21 19:58:10 +0000

    Protocol and version bumps

--- src/version.h
+++ src/version.h
@@ -20,11 +20,11 @@ static const int INIT_PROTO_VERSION = 209;
 static const int GETHEADERS_VERSION = 70077;
 
 //! disconnect from peers older than this proto version;
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70917;
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70918;
 static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT_2 = 70918;
 static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT_3 = 70919;
 
-static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70916;
+static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70917;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
